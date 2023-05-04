dc5bf6b2d0fb2b09cd93cf2d23f1af49624a5801
Arellano Jann <89598304+Arellano-Jann@users.noreply.github.com>
2023-05-04 15:25:31 -0700

    added defines

---
+++ headers/directory.h
@@ -0,0 +1,3 @@
+#pragma once
+#define DIRECTORY_H
+
---
+++ headers/file.h
@@ -0,0 +1,17 @@
+#pragma once
+#define FILE_H
+
+class File{
+    int num;
+
+public:
+    File(){
+
+    }
+
+    void setNum(int newNum){
+        num = newNum;
+    }
+
+    
+}
