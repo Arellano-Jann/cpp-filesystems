e118f22b2ed5005fa2f79c279b45041b13140fb6
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-05-05 09:34:23 -0700

    fixed filesize issue. need to fix ordering

--- src/directory.cpp
+++ src/directory.cpp
@@ -7,28 +7,35 @@ int dirExists(const char* const path) {
     return (statRC == 0) ? 1 : 0; // 1 if exists, 0 if not
 }
 
+long getFileSize(std::string filename){
+    struct stat info;
+    int rc = stat(filename.c_str(), &info);
+    return rc == 0 ? info.st_size : -2;
+}
+
 int Directory::createDirectory(){ // getpwuid, getuid, c_str()
     std::string homeDir = (getenv("HOME")) ? getenv("HOME") : getpwuid(getuid())->pw_dir; // sets the home directory if there isn't a default
 
     path = homeDir + "/" + directoryName;
 
     if (dirExists(path.c_str())) { // checks if directory already exists
-        std::cout << "Directory already exists. Loading directory" << std::endl;
+        printf("Loading existing directory\n\n");
         return 1;
     }
 
     // creates directory
     int status = mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
     if (status == 0) {
-        std::cout << "Directory created." << std::endl;
+        printf("Directory created.\n\n");
         return 0;
     }
     else {
-        std::cout << "Error creating directory." << std::endl;
+        printf("Error creating directory.\n\n");
         return 1;
     }
 }
 
+// doesn't order .. and . properly
 int Directory::displayDirectoryContent(){
     DIR *dir = opendir (path.c_str());
     struct dirent *entry;
@@ -37,12 +44,14 @@ int Directory::displayDirectoryContent(){
     if (dir == NULL) { return 1; }
 
     // print directory name
-    printf("%s", directoryName.c_str()); // better than printf(directoryName.c_str()) bc it's more secure
+    printf("%s\n", directoryName.c_str()); // better than printf(directoryName.c_str()) bc it's more secure
 
     // print all files in directory
     int i = 1;
     while ((entry = readdir(dir)) != NULL) {
-        printf("\t%i. %s %i bytes\n", i, entry->d_name, entry->d_reclen);
+        long fileSize = getFileSize(path + "/" + entry->d_name);
+        
+        printf("\t%i. %s %li bytes\n", i, entry->d_name, fileSize);
         i++;
     }
     printf("\t%i. %s\n\n", i, "Nevermind");
--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,14 @@ int getUserInput(){
 }
 
 int main(){
-    int i = 1;
-    char d_name[256] = "stuff";
-    unsigned short d_reclen = 10;
-    printf("\t%i. %s %i bytes\n", i, d_name, d_reclen);
-    i++;
-    printf("\t%i. %s\n\n", i, "Nevermind");
+    // int i = 1;
+    // char d_name[256] = "stuff";
+    // unsigned short d_reclen = 10;
+    // printf("\t%i. %s %i bytes\n", i, d_name, d_reclen);
+    // i++;
+    // printf("\t%i. %s\n\n", i, "Nevermind");
+
+    Directory d("test");
+    d.createDirectory();
+    d.displayDirectoryContent();
 }
